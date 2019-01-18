1557f160f31e5c33735648251b31a3a1e08c7ccd
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2019-01-18 00:51:07 -0200

    refactor: full refactor for iotagent

--- src/config.js
+++ src/config.js
@@ -8,9 +8,9 @@ config.backend_port = process.env.BACKEND_PORT || 6379;
 config.allow_unsecured_mode = process.env.ALLOW_UNSECURED_MODE || 'false';
 
 config.mosca_tls = {
-    cert: process.env.MOSCA_TLS_SECURE_CERT || '/opt/iot-agent/mosca/certs/mosca.crt',
-    key: process.env.MOSCA_TLS_SECURE_KEY || '/opt/iot-agent/mosca/certs/mosca.key',
-    ca: process.env.MOSCA_TLS_CA_CERT || '/opt/iot-agent/mosca/certs/ca.crt'
+    cert: process.env.MOSCA_TLS_SECURE_CERT || './mosca/certs/mosca.crt',
+    key: process.env.MOSCA_TLS_SECURE_KEY || './mosca/certs/mosca.key',
+    ca: process.env.MOSCA_TLS_CA_CERT || './mosca/certs/ca.crt'
 };
 
 module.exports = config;
