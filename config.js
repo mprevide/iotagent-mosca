e7b06526ee79ef8c6fe08d9f38ef3d9ea9bc9398
Giovanni Curiel dos Santos <giovannicuriel@gmail.com>
2019-03-22 11:21:12 -0300

    feat: implementing healthcheck

--- config.js
+++ config.js
@@ -13,4 +13,14 @@ config.mosca_tls = {
     ca: process.env.MOSCA_TLS_CA_CERT || '/opt/iot-agent/mosca/certs/ca.crt'
 };
 
+config.healthcheck = {
+    timeout: {
+        uptime: process.env.HC_UPTIME_TIMEOUT || 300000,
+        memory: process.env.HC_MEMORY_USAGE_TIMEOUT || 300000,
+        cpu: process.env.HC_CPU_USAGE_TIMEOUT || 300000,
+        mongodb: process.env.HC_MONGODB_TIMEOUT || 30000,
+        kafka: process.env.HC_KAFKA_TIMEOUT || 30000
+    }
+};
+
 module.exports = config;
