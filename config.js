bab6920e185507b85e512ffa412a718fdec71c82
Mariane Previde <mprevide@cpqd.com.br>
2019-06-28 08:35:17 -0300

    fix some lint warning and add eslint to project

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-'use strict';
 
-var config = {};
+
+const config = {};
 
 config.backend_host = process.env.BACKEND_HOST || 'mosca-redis';
 config.backend_port = process.env.BACKEND_PORT || 6379;
@@ -10,7 +10,7 @@ config.allow_unsecured_mode = process.env.ALLOW_UNSECURED_MODE || 'false';
 config.mosca_tls = {
     cert: process.env.MOSCA_TLS_SECURE_CERT || '/opt/iot-agent/mosca/certs/mosca.crt',
     key: process.env.MOSCA_TLS_SECURE_KEY || '/opt/iot-agent/mosca/certs/mosca.key',
-    ca: process.env.MOSCA_TLS_CA_CERT || '/opt/iot-agent/mosca/certs/ca.crt'
+    ca: process.env.MOSCA_TLS_CA_CERT || '/opt/iot-agent/mosca/certs/ca.crt',
 };
 
 config.healthcheck = {
@@ -19,8 +19,8 @@ config.healthcheck = {
         memory: process.env.HC_MEMORY_USAGE_TIMEOUT || 300000,
         cpu: process.env.HC_CPU_USAGE_TIMEOUT || 300000,
         mongodb: process.env.HC_MONGODB_TIMEOUT || 30000,
-        kafka: process.env.HC_KAFKA_TIMEOUT || 30000
-    }
+        kafka: process.env.HC_KAFKA_TIMEOUT || 30000,
+    },
 };
 
 module.exports = config;
--- healthcheck.js
+++ healthcheck.js
@@ -1,9 +1,7 @@
-"use strict";
-
-var os = require("os")
-var pjson = require ('./package.json');
-var config = require("./config");
-var HealthCheck = require("@dojot/healthcheck");
+const os = require('os');
+const HealthCheck = require('@dojot/healthcheck');
+const pjson = require('./package.json');
+const config = require('./config');
 
 
 /**
@@ -13,175 +11,167 @@ var HealthCheck = require("@dojot/healthcheck");
  * connections to services and resources used (CPU and memory)
  */
 class AgentHealthChecker {
-  constructor(kafkaMessenger, redisClient) {
-    const configHealth = {
-      description: 'Health status for IoT agent - MQTT',
-      version: pjson.version,
-      status: 'pass'
-    };
+    constructor(kafkaMessenger, redisClient) {
+        const configHealth = {
+            description: 'Health status for IoT agent - MQTT',
+            version: pjson.version,
+            status: 'pass',
+        };
 
-    this.healthChecker = new HealthCheck.HealthChecker(configHealth);
-    this.router = HealthCheck.getHTTPRouter(this.healthChecker);
-    this.kafkaMessenger = kafkaMessenger;
-    this.redisClient = redisClient;
-  }
-  /**
-   * Register all monitors
-   */
-  init() {
-    this._registerUptimeMonitor();
-    this._registerMemoryMonitor();
-    this._registerCpuMonitor();
-    this._registerKafkaMonitor();
-    this._registerRedisMonitor();
-  }
+        this.healthChecker = new HealthCheck.HealthChecker(configHealth);
+        this.router = HealthCheck.getHTTPRouter(this.healthChecker);
+        this.kafkaMessenger = kafkaMessenger;
+        this.redisClient = redisClient;
+    }
 
-  _registerUptimeMonitor() {
-    // uptime
-    const uptime = {
-      measurementName: 'uptime',
-      componentType: 'system',
-      observedUnit: 's',
-      status: 'pass'
-    };
+    /**
+     * Register all monitors
+     */
+    init() {
+        this._registerUptimeMonitor();
+        this._registerMemoryMonitor();
+        this._registerCpuMonitor();
+        this._registerKafkaMonitor();
+        this._registerRedisMonitor();
+    }
 
-    const collectUptime = (trigger = HealthCheck.DataTrigger) => {
-      let value = Math.floor(process.uptime());
-      trigger.trigger(value, 'pass');
-      return value;
-    };
+    _registerUptimeMonitor() {
+        // uptime
+        const uptime = {
+            measurementName: 'uptime',
+            componentType: 'system',
+            observedUnit: 's',
+            status: 'pass',
+        };
 
-    this.healthChecker.registerMonitor(uptime, collectUptime,
-      config.healthcheck.timeout.uptime);
-  }
+        const collectUptime = (trigger = HealthCheck.DataTrigger) => {
+            const value = Math.floor(process.uptime());
+            trigger.trigger(value, 'pass');
+            return value;
+        };
 
-  _registerMemoryMonitor() {
-    const memory = {
-      componentName: 'memory',
-      componentType: 'system',
-      measurementName: 'utilization',
-      observedUnit: 'percent',
-      status: 'pass'
-    };
+        this.healthChecker.registerMonitor(uptime, collectUptime,
+            config.healthcheck.timeout.uptime);
+    }
 
-    const collectMemory = (trigger = HealthCheck.DataTrigger) => {
-      let tmem = os.totalmem();
-      let fmem = os.freemem();
-      let pmem = (100 - (fmem / tmem) * 100).toFixed(2);
-      if (pmem > 75) {
-        trigger.trigger(pmem, 'warn');
-      }
-      else {
-        trigger.trigger(pmem, 'pass');
-      }
-      return pmem;
-    };
+    _registerMemoryMonitor() {
+        const memory = {
+            componentName: 'memory',
+            componentType: 'system',
+            measurementName: 'utilization',
+            observedUnit: 'percent',
+            status: 'pass',
+        };
 
-    this.healthChecker.registerMonitor(memory, collectMemory,
-      config.healthcheck.timeout.memory);
-  }
+        const collectMemory = (trigger = HealthCheck.DataTrigger) => {
+            const tmem = os.totalmem();
+            const fmem = os.freemem();
+            const pmem = (100 - (fmem / tmem) * 100).toFixed(2);
+            if (pmem > 75) {
+                trigger.trigger(pmem, 'warn');
+            } else {
+                trigger.trigger(pmem, 'pass');
+            }
+            return pmem;
+        };
 
-  _registerCpuMonitor() {
-    const cpu = {
-      componentName: 'cpu',
-      componentType: 'system',
-      measurementName: 'utilization',
-      observedUnit: 'percent',
-      status: 'pass'
-    };
+        this.healthChecker.registerMonitor(memory, collectMemory,
+            config.healthcheck.timeout.memory);
+    }
 
-
-    const collectCpu = (trigger = HealthCheck.DataTrigger) => {
-      let ncpu = os.cpus().length;
-      let lcpu = os.loadavg()[1]; //last five minute
-      let pcpu = (100 * lcpu / ncpu).toFixed(2);
-      if (pcpu > 75) {
-        trigger.trigger(pcpu, 'warn');
-      }
-      else {
-        trigger.trigger(pcpu, 'pass');
-      }
-      return pcpu;
-    };
-
-    this.healthChecker.registerMonitor(cpu, collectCpu,
-      config.healthcheck.timeout.cpu);
-  }
-
-  _registerRedisMonitor() {
-    const redisInfo = {
-      componentName: 'redis',
-      componentType: 'datastore',
-      measurementName: 'accessibility',
-      observedUnit: 'boolean',
-      status: 'pass'
-    };
-
-    const dataTrigger = this.healthChecker.registerMonitor(redisInfo);
-    this.redisClient.on("ready", () => {
-      dataTrigger.trigger(true, "pass");
-    });
-
-    this.redisClient.on("end", () => {
-      dataTrigger.trigger(false, "fail");
-    });
-  }
+    _registerCpuMonitor() {
+        const cpu = {
+            componentName: 'cpu',
+            componentType: 'system',
+            measurementName: 'utilization',
+            observedUnit: 'percent',
+            status: 'pass',
+        };
 
 
-  _getKafkaStatus() {
-    return new Promise((resolve, reject) => {
-      let kafkaStatus = {
-        connected: false
-      };
+        const collectCpu = (trigger = HealthCheck.DataTrigger) => {
+            const ncpu = os.cpus().length;
+            const lcpu = os.loadavg()[1]; // last five minute
+            const pcpu = (100 * lcpu / ncpu).toFixed(2);
+            if (pcpu > 75) {
+                trigger.trigger(pcpu, 'warn');
+            } else {
+                trigger.trigger(pcpu, 'pass');
+            }
+            return pcpu;
+        };
 
-      // It can be the consumer or the producer because the returned value is the same.
-      this.kafkaMessenger.consumer.consumer.getMetadata({ timeout: 3000 },
-        (error, metadata) => {
-          if (error) {
-            reject(new Error('Internal error while getting kafka metadata.'));
-          }
-          else {
-            kafkaStatus.connected = true;
-            kafkaStatus.details = {
-              metadata: metadata
-            };
-            resolve(kafkaStatus);
-          }
+        this.healthChecker.registerMonitor(cpu, collectCpu,
+            config.healthcheck.timeout.cpu);
+    }
+
+    _registerRedisMonitor() {
+        const redisInfo = {
+            componentName: 'redis',
+            componentType: 'datastore',
+            measurementName: 'accessibility',
+            observedUnit: 'boolean',
+            status: 'pass',
+        };
+
+        const dataTrigger = this.healthChecker.registerMonitor(redisInfo);
+        this.redisClient.on('ready', () => {
+            dataTrigger.trigger(true, 'pass');
         });
-    });
-  }
 
-  _registerKafkaMonitor() {
-
-    // kafka:connections
-    const kafka = {
-      componentName: 'kafka',
-      componentType: 'datastore',
-      measurementName: 'accessibility',
-      observedUnit: 'boolean',
-      status: 'pass'
-    };
+        this.redisClient.on('end', () => {
+            dataTrigger.trigger(false, 'fail');
+        });
+    }
 
 
-    const collectKafkaState = (trigger = HealthCheck.DataTrigger) => {
-      return this._getKafkaStatus().then(status => {
-        if (status.connected) {
-          trigger.trigger(1 /*one connection */, 'pass');
-        }
-        else {
-          trigger.trigger(0 /* zero connection */, 'fail');
-        }
-      }).catch(error => {
-        trigger.trigger(0 /* zero connection */, 'fail', error);
-      });
-    };
+    _getKafkaStatus() {
+        return new Promise((resolve, reject) => {
+            const kafkaStatus = {
+                connected: false,
+            };
+
+            // It can be the consumer or the producer because the returned value is the same.
+            this.kafkaMessenger.consumer.consumer.getMetadata({ timeout: 3000 },
+                (error, metadata) => {
+                    if (error) {
+                        reject(new Error('Internal error while getting kafka metadata.'));
+                    } else {
+                        kafkaStatus.connected = true;
+                        kafkaStatus.details = {
+                            metadata,
+                        };
+                        resolve(kafkaStatus);
+                    }
+                });
+        });
+    }
+
+    _registerKafkaMonitor() {
+        // kafka:connections
+        const kafka = {
+            componentName: 'kafka',
+            componentType: 'datastore',
+            measurementName: 'accessibility',
+            observedUnit: 'boolean',
+            status: 'pass',
+        };
 
 
-    this.healthChecker.registerMonitor(kafka, collectKafkaState,
-      config.healthcheck.timeout.kafka);
-
-  }
+        const collectKafkaState = (trigger = HealthCheck.DataTrigger) => {
+            this._getKafkaStatus().then((status) => {
+                if (status.connected) {
+                    trigger.trigger(1 /* one connection */, 'pass');
+                } else {
+                    trigger.trigger(0 /* zero connection */, 'fail');
+                }
+            }).catch((error) => {
+                trigger.trigger(0 /* zero connection */, 'fail', error);
+            });
+        };
+        this.healthChecker.registerMonitor(kafka, collectKafkaState,
+            config.healthcheck.timeout.kafka);
+    }
 }
 
 module.exports = AgentHealthChecker;
-
