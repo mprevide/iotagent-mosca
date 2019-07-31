79b8a6b3a3ca8f89f8f9284479875d411cd858c2
joab-cpqd <joab@cpqd.com.br>
2019-07-31 16:51:10 -0300

    Testing endpoint to get metrics object

--- tests/unit-tests/metrics.test.js
+++ tests/unit-tests/metrics.test.js
@@ -6,26 +6,59 @@
  *
  * This module has the following dependencies:
  *
- * - express
+ * - supertest
  */
 
-const express = require("express");
+// const request = require("request");
+const app = require("../../src/app").app;
+const stopApp = require("../../src/app").stopApp;
+
+const request = require("supertest");
 const moscaMestrics = require("../../src/metrics");
 
 //
 // Mocking dependencies
 //
-jest.mock("express");
 
-describe("Testing payload", () => {
+// a helper function to make a POST request
+function get(url, body) {
+    const httpRequest = request(app).get(url);
+    httpRequest.send(body);
+    httpRequest.set('Accept', 'application/json');
+    return httpRequest;
+}
+
+describe("Testing metrics functions", () => {
+
     beforeEach(() => {
-        express.Router.mockClear();
+        const metricStore = new moscaMestrics.Metrics();
+            metricStore.lastMetricsInfo = {
+            connectedClients: 0,
+            connectionsLoad1min: 0,
+            connectionsLoad5min: 0,
+            connectionsLoad15min: 0,
+            messagesLoad1min: 0,
+            messagesLoad5min: 0,
+            messagesLoad15min: 0
+        };
+
+         app.use(moscaMestrics.getHTTPRouter(metricStore));
+    });
+
+    afterEach(() => {
+        stopApp();
+    });
+
+    it('Should return OK status and the metricStore object', async () => {
+        const response = await get('/iotagent-mqtt/metrics', '');
+
+        expect(response.status).toEqual(200);
+        expect(JSON.parse(response.text)).toEqual({"connectedClients": 0, "connectionsLoad1min": 0, "connectionsLoad5min": 0, "connectionsLoad15min": 0, "messagesLoad1min": 0, "messagesLoad5min": 0, "messagesLoad15min": 0});
     });
 
 
     it("Should return NULL because function expects two strings as argument", () => {
         const metrics = new moscaMestrics.Metrics();
-
         metrics.preparePayloadObject()
         expect(metrics.lastMetricsInfo.connectedClients).toBeNull();
 
@@ -34,13 +67,6 @@ describe("Testing payload", () => {
 
         const metricsAttribute = "connectedClients";
         metrics.preparePayloadObject(metricsAttribute, 20);
-        expect(metrics.lastMetricsInfo[`${metricsAttribute}`]).toEqual('20');
+        expect(metrics.lastMetricsInfo[`${metricsAttribute}`]).toEqual("20");
     });
-
-    it("Should get metrics object from endpoint", (done) => {
-        const metrics = new moscaMestrics.Metrics();
-        metrics.getHTTPRouter = jest.fn();
-
-        expect(metrics.getHTTPRouter()).toBeUndefined();
-    });
-})
+});
