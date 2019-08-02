6da00fe89ba60bcbdfb44d495ccbf551fc52ee1c
Mariane Previde <mprevide@cpqd.com.br>
2019-08-02 18:07:36 -0300

    to test

--- certUtils.py
+++ certUtils.py
@@ -9,6 +9,12 @@ from OpenSSL import crypto
 
 # the following functions are related to file manipulation
 # these functions  may throw crypto.Error
+# change to (https://stackoverflow.com/questions/2657693/insert-a-newline-character-every-64-characters-using-python)
+# def saveCRL(filename, rawCRL):
+# crl = ("-----BEGIN X509 CRL-----\n"
+# + '\n'.join(rawCRL[i:i+64] for i in range(0, len(rawCRL), 64)) #X509 -each line have 64 bytes
+# + "\n-----END X509 CRL-----\n")
+# crypto.load_crl(crypto.FILETYPE_PEM, crl)
 
 def saveCRL(filename, rawCRL):
     crl = ("-----BEGIN X509 CRL-----\n"
@@ -20,7 +26,7 @@ def saveCRL(filename, rawCRL):
     with open(filename, "w") as crlFile:
         crlFile.write(crl)
 
-    
+
 
 
 
