1913e3bda7ab13f1850f33a62b7e7e680891bb00
codefactor-io <support@codefactor.io>
2019-08-29 18:31:10 +0000

    [CodeFactor] Apply fixes to commit c3f13ba
    [ci skip] [skip ci]

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 /* private */
 const unsecured_mode = (mode) => ((mode || false) && (mode.toLowerCase().trim() === "true" || Number(mode) > 0));
-const _zeroIsDisabled = (envValue, defaultValue) => (envValue===0 || envValue==='0' ? null : (Number(envValue) ? Number(envValue) : defaultValue) );
+const _zeroIsDisabled = (envValue, defaultValue) => (envValue === 0 || envValue === '0' ? null : (Number(envValue) ? Number(envValue) : defaultValue));
 
 /* public */
 var config = {};
