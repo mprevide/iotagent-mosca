9a67beac364e5e96f716ac48b6312e09fb2d9d6e
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2020-05-18 10:52:08 -0300

    custom kafka params

--- src/config.js
+++ src/config.js
@@ -49,6 +49,70 @@ config.healthcheck = {
     }
 };
 
+config.kafka = {
+    producer: {
+        "metadata.broker.list": process.env.KAFKA_HOSTS || "kafka:9092",
+        "compression.codec": "gzip",
+        "retry.backoff.ms": 200,
+        "message.send.max.retries": 10,
+        "socket.keepalive.enable": true,
+        "queue.buffering.max.messages": 100000,
+        "queue.buffering.max.ms": 100,
+        "batch.num.messages": 1000000,
+        "dr_cb": true
+    },
+
+    consumer: {
+        "group.id": process.env.KAFKA_GROUP_ID || "iotagent-mqtt-group",
+        "metadata.broker.list": process.env.KAFKA_HOSTS || "kafka:9092",
+    },
+    dojot: {
+        subscriptionHoldoff: Number(process.env.DOJOT_SUBSCRIPTION_HOLDOFF) || 2500,
+        timeoutSleep: 5,
+        connectionRetries: 5
+    }
+};
+
+config.databroker = {
+    url: process.env.DATA_BROKER_URL || "http://data-broker",
+    timeoutSleep: 2,
+    connectionRetries: 5,
+};
+
+config.auth = {
+    url: process.env.AUTH_URL || "http://auth:5000",
+    timeoutSleep: 5,
+    connectionRetries: 5,
+};
+
+config.deviceManager = {
+    url: process.env.DEVICE_MANAGER_URL || "http://device-manager:5000",
+    timeoutSleep: 5,
+    connectionRetries: 3,
+};
+
+config.dojot = {
+    management: {
+        user: process.env.DOJOT_MANAGEMENT_USER || "dojot-management",
+        tenant: process.env.DOJOT_MANAGEMENT_TENANT || "dojot-management"
+    },
+    subjects: {
+        tenancy: process.env.DOJOT_SUBJECT_TENANCY || "dojot.tenancy",
+        devices: process.env.DOJOT_SUBJECT_DEVICES || "dojot.device-manager.device",
+        deviceData: process.env.DOJOT_SUBJECT_DEVICE_DATA || "device-data",
+    },
+    events: {
+        tenantEvent: {
+        NEW_TENANT: "new-tenant",
+        DELETE_TENANT: "delete-tenant"
+        },
+        tenantActionType: {
+        CREATE: "create",
+        DELETE: "delete"
+        }
+    }
+};
+
 config.deviceToDojotPayloadSize = process.env.DEV_TO_DOJOT_PAYLOAD_MAX_SIZE || 256000000;
 config.DojotToDevicePayloadSize = process.env.DOJOT_TO_DEV_PAYLOAD_MAX_SIZE || 256000000;
 
